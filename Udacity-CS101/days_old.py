d163378ce229199d8da67f8bef613dac7d81b92d
q-cai <petercaiq@gmail.com>
2017-10-22 21:58:47 -0400

    Clean code
    Cleaned code and used another way to solve the problem with Lesson 10

--- Udacity-CS101/days_old.py
+++ Udacity-CS101/days_old.py
@@ -1,14 +1,24 @@
-# By Websten from forums
-#
+# Learn from Lesson 10:
+
+# Understand inputs --> Understand outputs -->
+# Understand relationship -- examples --> simple mechanical solution -->
+# Develop incrementally and test as we go
+
+# Program defensively! Add assertions if the input is not valid!
+# Custom for naming a variable: don't start with a caption letter
+
+
 # Given your birthday and the current date, calculate your age in days.
 # Account for leap days.
 #
 # Assume that the birthday and current date are correct dates (and no
 # time travel).
-#
+
+
+
 daysofMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-def ISleapyear(year):
+def isLeapYear(year):
     result = False
     if(year % 4 == 0 and year % 100 != 0):
         result = True
@@ -16,19 +26,19 @@ def ISleapyear(year):
         result = True
     return(result)
 
-def DaysforMonths(month, year):
+def daysInMonths(month, year):
     x = 0
     mon = 0
     for mon in range(0, month - 1):
         x += daysofMonths[mon]
-        if(mon == 1 and ISleapyear(year)):
+        if(mon == 1 and isLeapYear(year)):
             x += 1
     return x
 
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
-    result = (year2 - year1) * 365 + DaysforMonths(month2, year2) - DaysforMonths(month1, year1) + day2 - day1
+    result = (year2 - year1) * 365 + daysInMonths(month2, year2) - daysInMonths(month1, year1) + day2 - day1
     for year in range(year1 + 1, year2):
-        if(ISleapyear(year)):
+        if(isLeapYear(year)):
             result += 1
     return(result)
 
---
+++ Udacity-CS101/days_old2.py
@@ -0,0 +1,71 @@
+# A different way to solve this problem
+# by adopting the idea in Lesson 10
+#
+# Use Dave's suggestions to finish your daysBetweenDates
+# procedure. It will need to take into account leap years
+# in addition to the correct number of days in each month.
+
+daysofMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def isLeapYear(year):
+    result = False
+    if(year % 4 == 0 and year % 100 != 0):
+        result = True
+    if(year % 400 == 0):
+        result = True
+    return(result)
+
+def daysInMonths(month, year):
+    if(isLeapYear(year) and month ==2):
+        return(29)
+    else:
+        return(daysofMonths[month - 1])
+
+def nextDay(year, month, day):
+    if(day < daysInMonths(month, year)):
+        return(year, month, day + 1)
+    else:
+        if(month == 12):
+            return(year + 1, 1, 1)
+        else:
+            return(year, month + 1, 1)
+
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before year2-month2-day2. Otherwise, returns False."""
+    if(year1 < year2):
+        return True
+    if(year1 == year2):
+        if(month1 < month2):
+            return True
+        if(month1 == month2):
+            return day1 < day2
+    return False
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar."""
+
+    # program defensively! Add an assertion if the input is not valid!
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+
+    days = 0
+    while(dateIsBefore(year1, month1, day1, year2, month2, day2)):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return(days)
+
+def test():
+    assert daysBetweenDates(2013, 1, 1, 2013, 1, 1) == 0
+    assert daysBetweenDates(2013, 1, 1, 2013, 1, 2) == 1
+    assert daysBetweenDates(2013, 1, 1, 2014, 1, 1) == 365
+    assert isLeapYear(2001) == False
+    assert daysInMonths(2, 2001) == 28
+    assert daysInMonths(2, 2000) == 29
+    assert nextDay(2013, 1, 1) == (2013, 1, 2)
+    assert nextDay(2012, 2, 28) == (2012, 2, 29)
+    assert nextDay(2013, 12, 31) == (2014, 1, 1)
+    assert nextDay(2001, 2, 28) == (2001, 3, 1)
+    print('tese finished.')
+
+test()
